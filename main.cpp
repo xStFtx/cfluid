bb266dba7367635aea793ff3b7b621661d39e4ca
xStFtx <you@example.com>
2023-10-08 16:34:17 +0200

    1.2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
-const int Lx = 100;   // Grid size in the x-direction
-const int Ly = 50;    // Grid size in the y-direction
-const int Q = 9;      // Number of velocity directions
-const double tau = 0.6; // Relaxation time
+const int Lx = 100;          // Grid size in the x-direction
+const int Ly = 50;           // Grid size in the y-direction
+const int Q = 9;             // Number of velocity directions
+const double tau = 0.6;      // Relaxation time
+const double density_in = 1.0;
+const double density_out = 0.0;
+const double initial_velocity = 0.1;
 
 class FluidSimulation {
 public:
@@ -15,14 +19,24 @@ public:
     // Initialize the simulation
     void initialize() {
         // Initialize density, velocity, and distribution functions
-        density.assign(Lx, std::vector<double>(Ly, 1.0));
+        density.assign(Lx, std::vector<double>(Ly, density_in));
         ux.assign(Lx, std::vector<double>(Ly, 0.0));
         uy.assign(Lx, std::vector<double>(Ly, 0.0));
         f.assign(Lx, std::vector<std::vector<double>>(Ly, std::vector<double>(Q, 0.0)));
+
+        // Set initial velocity in the x-direction
+        for (int x = 0; x < Lx; x++) {
+            for (int y = 0; y < Ly; y++) {
+                ux[x][y] = initial_velocity;
+            }
+        }
     }
 
     // Perform a single time step of the simulation
     void step() {
+        // Calculate macroscopic variables (density and velocity)
+        calculateMacroscopicVariables();
+
         // Collision step
         collisionStep();
 
@@ -43,60 +57,54 @@ private:
     std::vector<std::vector<std::vector<double>>> f;
 
     // Helper functions for collision and streaming steps
+    void calculateMacroscopicVariables() {
+        for (int x = 0; x < Lx; x++) {
+            for (int y = 0; y < Ly; y++) {
+                density[x][y] = 0.0;
+                ux[x][y] = 0.0;
+                uy[x][y] = 0.0;
+                for (int k = 0; k < Q; k++) {
+                    density[x][y] += f[x][y][k];
+                    ux[x][y] += f[x][y][k] * c[k][0];
+                    uy[x][y] += f[x][y][k] * c[k][1];
+                }
+                ux[x][y] /= density[x][y];
+                uy[x][y] /= density[x][y];
+            }
+        }
+    }
+
     void collisionStep() {
         for (int x = 0; x < Lx; x++) {
             for (int y = 0; y < Ly; y++) {
-                double local_density = 0.0;
+                double local_density = density[x][y];
+                double usqr = ux[x][y] * ux[x][y] + uy[x][y] * uy[x][y];
                 for (int k = 0; k < Q; k++) {
-                    local_density += f[x][y][k];
-                }
-
-                double cx, cy, cu, usqr;
-                for (int k = 0; k < Q; k++) {
-                    cx = c[k][0];
-                    cy = c[k][1];
-                    cu = cx * ux[x][y] + cy * uy[x][y];
-                    usqr = ux[x][y] * ux[x][y] + uy[x][y] * uy[x][y];
-                    f[x][y][k] = f[x][y][k] - (f[x][y][k] - feq(k, x, y, local_density) * (1.0 + 3.0 * cu + 9.0 / 2.0 * cu * cu - 3.0 / 2.0 * usqr)) / tau;
+                    double cu = c[k][0] * ux[x][y] + c[k][1] * uy[x][y];
+                    double feq_val = feq(k, local_density, ux[x][y], uy[x][y], usqr);
+                    f[x][y][k] = f[x][y][k] - (f[x][y][k] - feq_val) / tau;
                 }
             }
         }
     }
 
     void streamingStep() {
-        for (int x = Lx - 1; x > 0; x--) {
+        std::vector<std::vector<std::vector<double>>> f_new(Lx, std::vector<std::vector<double>>(Ly, std::vector<double>(Q, 0.0)));
+        for (int x = 0; x < Lx; x++) {
             for (int y = 0; y < Ly; y++) {
                 for (int k = 0; k < Q; k++) {
-                    int new_x = x + c[k][0];
-                    int new_y = y + c[k][1];
-
-                    if (new_x < 0) {
-                        new_x = Lx - 1;
-                    }
-                    else if (new_x >= Lx) {
-                        new_x = 0;
-                    }
-
-                    if (new_y < 0) {
-                        new_y = 0;
-                    }
-                    else if (new_y >= Ly) {
-                        new_y = Ly - 1;
-                    }
-
-                    f[new_x][new_y][k] = f[x][y][k];
+                    int new_x = (x + c[k][0] + Lx) % Lx;
+                    int new_y = (y + c[k][1] + Ly) % Ly;
+                    f_new[new_x][new_y][k] = f[x][y][k];
                 }
             }
         }
+        f = f_new;
     }
 
-    double feq(int k, int x, int y, double local_density) {
-        double cx = c[k][0];
-        double cy = c[k][1];
-        double usqr = ux[x][y] * ux[x][y] + uy[x][y] * uy[x][y];
-        double cu = cx * ux[x][y] + cy * uy[x][y];
-
-        return w[k] * local_density * (1.0 + 3.0 * cu + 9.0 / 2.0 * cu * cu - 3.0 / 2.0 * usqr);
+    double feq(int k, double rho, double u_x, double u_y, double usqr) {
+        double cu = c[k][0] * u_x + c[k][1] * u_y;
+        return w[k] * rho * (1.0 + 3.0 * cu + 9.0 / 2.0 * cu * cu - 3.0 / 2.0 * usqr);
     }
 
     // Lattice weights and directions (D2Q9 model)
