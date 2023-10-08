ac5a33f9193e77872fe973d7702310122f13bd11
xStFtx <you@example.com>
2023-10-08 16:39:38 +0200

    1.3

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <fstream>
 #include <omp.h>
 
-const int Lx = 100;          // Grid size in the x-direction
-const int Ly = 50;           // Grid size in the y-direction
-const int Q = 9;             // Number of velocity directions
-const double tau = 0.6;      // Relaxation time
+const int Lx = 100;
+const int Ly = 50;
+const int Q = 9;
+const double tau = 0.6;
 const double density_in = 1.0;
 const double density_out = 0.0;
 const double initial_velocity = 0.1;
@@ -18,15 +18,12 @@ public:
         initialize();
     }
 
-    // Initialize the simulation
     void initialize() {
-        // Initialize density, velocity, and distribution functions
         density.assign(Lx, std::vector<double>(Ly, density_in));
         ux.assign(Lx, std::vector<double>(Ly, 0.0));
         uy.assign(Lx, std::vector<double>(Ly, 0.0));
         f.assign(Lx, std::vector<std::vector<double>>(Ly, std::vector<double>(Q, 0.0)));
 
-        // Set initial velocity in the x-direction
         for (int x = 0; x < Lx; x++) {
             for (int y = 0; y < Ly; y++) {
                 ux[x][y] = initial_velocity;
@@ -34,22 +31,13 @@ public:
         }
     }
 
-    // Perform a single time step of the simulation
     void step() {
-        // Calculate macroscopic variables (density and velocity)
         calculateMacroscopicVariables();
-
-        // Collision step
         collisionStep();
-
-        // Streaming step
         streamingStep();
     }
 
-    // Visualize the simulation results (simplified, does not provide actual visualization)
     void visualize() {
-        // Print or visualize the simulation results
-        // Example: You can write the density values to a file for visualization.
         std::ofstream outFile("density.dat");
         for (int x = 0; x < Lx; x++) {
             for (int y = 0; y < Ly; y++) {
@@ -60,13 +48,11 @@ public:
     }
 
 private:
-    // Grid data (density, velocity components, and distribution functions)
     std::vector<std::vector<double>> density;
     std::vector<std::vector<double>> ux;
     std::vector<std::vector<double>> uy;
     std::vector<std::vector<std::vector<double>>> f;
 
-    // Helper functions for collision and streaming steps
     void calculateMacroscopicVariables() {
         #pragma omp parallel for collapse(2)
         for (int x = 0; x < Lx; x++) {
@@ -120,7 +106,6 @@ private:
         return w[k] * rho * (1.0 + 3.0 * cu + 9.0 / 2.0 * cu * cu - 3.0 / 2.0 * usqr);
     }
 
-    // Lattice weights and directions (D2Q9 model)
     double w[Q] = {4.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0};
     int c[Q][2] = {{0, 0}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}};
 };
@@ -129,12 +114,10 @@ int main() {
     std::cout << "Running..." << std::endl;
     FluidSimulation simulation;
 
-    // Main time-stepping loop
     for (int t = 0; t < 1000; t++) {
         simulation.step();
     }
 
-    // Visualize the results
     simulation.visualize();
 
     return 0;
